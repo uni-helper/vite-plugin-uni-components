d8fb76a60f538027d0e96dce3d685edecf0d3c8e
FliPPeDround <734243792@qq.com>
2025-09-23 10:58:07 +0800

    docs(resolver): 为uView Pro解析器添加注释说明

--- packages/core/src/_resolvers/uview-pro.ts
+++ packages/core/src/_resolvers/uview-pro.ts
@@ -1,7 +1,18 @@
 import type { ComponentResolver } from '../types'
 
 import { kebabCase } from '../utils'
-
+/**
+ * uView Pro 组件解析器
+ * @param prefix - 组件导入路径的前缀，默认为 'uview-pro'
+ * @param [prefix='uview-pro'] - 可传入 uni_modules 路径以支持 uni_modules
+ * @returns uView Pro 组件的解析器函数
+ * @example
+ * // 基本用法
+ * uViewProResolver()
+ *
+ * // 支持 uni_modules
+ * uViewProResolver('@/uni_modules/uview-pro')
+ */
 export function uViewProResolver(prefix = 'uview-pro'): ComponentResolver {
   return {
     type: 'component',
